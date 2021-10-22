58984d065e0df60b57152a42bd8e8cb5145aa2e5
Simon <rhymesun@outlook.com>
2021-10-22 11:53:20 +0800

    refactor: 修改了静态资源路径配置

--- journey-blog/src/main/java/com/rhymesun/journey/blog/infrastructure/config/WebMvcConfig.java
+++ journey-blog/src/main/java/com/rhymesun/journey/blog/infrastructure/config/WebMvcConfig.java
@@ -2,8 +2,10 @@ package com.rhymesun.journey.blog.infrastructure.config;
 
 import java.time.Duration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -15,14 +17,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private static final String ASTERISK = "*";
+    private static final String PATH_PATTERN = "/**";
+
+    @Value("${storage.location}")
+    private String location;
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping(PATH_PATTERN)
                 .allowCredentials(true)
                 .allowedOriginPatterns(ASTERISK)
                 .allowedMethods(ASTERISK)
                 .allowedHeaders(ASTERISK)
                 .maxAge(Duration.ofHours(1).getSeconds());
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(PATH_PATTERN).addResourceLocations("file:" + location);
     }
 }
