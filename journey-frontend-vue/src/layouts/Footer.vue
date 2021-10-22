b96576eba4644fe1130831f7f8d51b544892ead5
Simon <rhymesun@outlook.com>
2021-10-22 16:53:15 +0800

    refactor: 页脚添加了备案信息

--- journey-frontend-vue/src/layouts/Footer.vue
+++ journey-frontend-vue/src/layouts/Footer.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>Journey ©2021 Created by Simon</div>
+  <div class="footer">
+    <span> Journey ©2021 Created by Simon·Portgas </span>
+    <a href="https://beian.miit.gov.cn/" target="_blank">陕ICP备2021013187号</a>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style>
+.footer > span {
+  margin-right: 10px;
+}
+</style>
